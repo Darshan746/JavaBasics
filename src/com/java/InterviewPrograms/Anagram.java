a987a9e52db574933fbef1584d1510cc06a4891e
darshan.mn746@gmail.com <darshan.mn746@gmail.com>
2018-02-13 21:03:00 +0530

    Anagram program

---
+++ src/com/java/InterviewPrograms/Anagram.java
@@ -0,0 +1,38 @@
+package com.java.InterviewPrograms;
+
+import java.util.Arrays;
+
+public class Anagram
+{
+
+    public static boolean isAnagram(String s1, String s2)
+    {
+        String copyS1 = s1.replaceAll("\\s", "").toLowerCase();
+        String copyS2 = s2.replaceAll("\\s", "").toLowerCase();
+
+        if (copyS1.length() == copyS2.length())
+        {
+            char[] charArray1 = copyS1.toCharArray();
+            char[] charArray2 = copyS2.toCharArray();
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            for (int i = 0; i < charArray1.length; i++)
+            {
+                if (charArray1[i] != charArray2[i])
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        return false;   
+        
+    }
+    public static void main(String[] args)
+    {
+       System.out.println(Anagram.isAnagram("keep ", "peke"));
+    }
+
+}
--- src/com/java/InterviewPrograms/SumOfDigits.java
+++ src/com/java/InterviewPrograms/SumOfDigits.java
@@ -2,21 +2,26 @@ package com.java.InterviewPrograms;
 
 import java.io.IOException;
 
-public class SumOfDigits {
-	
-	public static void sumOfDigits(int digits) throws IOException{
+public class SumOfDigits
+{
 
-		int a=0,sum=0,i;
-		while(digits!=0) {
-			a=digits%10;
-			digits=digits/10;
-			sum=sum+a;
-		}
-		System.out.println(sum);
-		
-	}
-	public static void main(String args[]) throws IOException{
-		sumOfDigits(321);
-	}
+    public static void sumOfDigits(int digits) throws IOException
+    {
+
+        int a = 0, sum = 0;
+        while (digits != 0)
+        {
+            a = digits % 10;
+            digits = digits / 10;
+            sum = sum + a;
+        }
+        System.out.println(sum);
+
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        sumOfDigits(321);
+    }
 
 }
