040868a25fbfd570f2eba5498ed7b237e2f94bdd
darshan.mn746@gmail.com <darshan.mn746@gmail.com>
2018-02-13 23:29:58 +0530

    palindrome

---
+++ src/com/java/InterviewPrograms/PalindromeNum.java
@@ -0,0 +1,20 @@
+package com.java.InterviewPrograms;
+
+public class PalindromeNum
+{
+
+    public static void main(String[] args)
+    {
+        int num = 231;
+        int temp = num;
+        StringBuilder b = new StringBuilder();
+        while (temp != 0)
+        {
+            b.append(temp % 10);
+            temp = temp / 10;
+        }
+        // System.out.println("hi");
+        int a = Integer.valueOf(b.toString());
+        System.out.println(a == num);
+    }
+}
