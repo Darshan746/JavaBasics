44761a5b15926f47cfd6fa14825cd6954786c043
Darshan <darshan.mn746@gmail.com>
2020-05-26 21:57:14 +0530

    some changes for program

--- src/com/java/arrays/LeaderInArray.java
+++ src/com/java/arrays/LeaderInArray.java
@@ -3,23 +3,25 @@ package com.java.arrays;
 //The element should be greater than its right side elements then its a Leader in the array
 public class LeaderInArray {
 
-    public void printLeader(int[] array) {
+    public static void printLeader(int[] array) {
         int j;
-        for(int i=0;i<array.length-1;i++) {
-            for ( j= i+1; j<array.length-1; j++){
+        for(int i=0;i<=array.length-1;i++) {
+            for ( j= i+1; j<=array.length-1; j++){
                 if (array[i] < array[j]){
                     break;
                 }
-                if(j==array.length)
-                System.out.println(array[i]);
+                if(j==array.length-1)
+                System.out.print(array[i]+" ");
             }
 
         }
+        System.out.println(array[array.length-1]);
     }
 
 
     public static void main(String[] args) {
-        
+        int[] array = {5,4,6,3,9,1};
+printLeader(array);
     }
 
 
