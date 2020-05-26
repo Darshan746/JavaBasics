543b6708f44065308b11cb377138ff6d5303e51c
Darshan <darshan.mn746@gmail.com>
2020-05-26 22:16:11 +0530

    some changes for program

--- src/com/java/arrays/LeaderInArray.java
+++ src/com/java/arrays/LeaderInArray.java
@@ -3,6 +3,8 @@ package com.java.arrays;
 //The element should be greater than its right side elements then its a Leader in the array
 public class LeaderInArray {
 
+
+    //this approach takes (n^2)
     public static void printLeader(int[] array) {
         int j;
         for(int i=0;i<=array.length-1;i++) {
@@ -11,17 +13,31 @@ public class LeaderInArray {
                     break;
                 }
                 if(j==array.length-1)
-                System.out.print(array[i]+" ");
+                    System.out.print(array[i]+" ");
             }
-
         }
         System.out.println(array[array.length-1]);
     }
 
 
+    public static void printLeaderElem(int[] array) {
+        //mfr -> maximum from right // assuming right most ele is greater
+        int mfr = array[array.length-1];
+        for(int i = array.length-2; i>=0; i--){
+            if(array[i]>mfr) {
+                System.out.print(array[i]+" ");
+                mfr = array[i];
+            }
+        }
+        //last element in the array always be a leader
+        System.out.println(array[array.length-1]);
+    }
+
+
     public static void main(String[] args) {
-        int[] array = {5,4,6,3,9,1};
-printLeader(array);
+        int[] array = {5,4,6,3,9,1,13};
+        // printLeader(array);
+        printLeaderElem(array);
     }
 
 
