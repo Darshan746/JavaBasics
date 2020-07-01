ac96c989adfc08e2fc7aef1bffacf5926ef881fa
Darshan <darshan.mn746@gmail.com>
2020-07-01 20:44:12 +0530

    some changes for program

--- src/com/java/String/StringBalanced.java
+++ src/com/java/String/StringBalanced.java
@@ -5,7 +5,13 @@ import java.util.Stack;
 //How to check if a String is balanced Expression
 public class StringBalanced {
 
-public static boolean isStringBalanced(String str) {
+    /**
+     * Push Any open Parantheses to the stack and if any closed symbols comes then
+     * pop the stack and compare the result if they are not same then the string is not balanced
+     * @param str
+     * @return
+     */
+    public static boolean isStringBalanced(String str) {
     Stack<Character> st = new Stack();
     for(char chr : str.toCharArray())
     {
